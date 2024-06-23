6e8e45e8e405dae166942ae069a85675d0128d19
Amitabh <sonuambastha117@gmail.com>
2024-06-23 13:22:45 +0530

    err corre

--- src/components/ImageCarousal.tsx
+++ src/components/ImageCarousal.tsx
@@ -7,52 +7,56 @@ const ImageCarousal = () => {
     data: products,
   } = useGetProductsQuery("productApi");
 
-  const DATA_LENGTH = products.length;
+  const DATA_LENGTH = products?.length || 0;
 //   console.log(DATA_LENGTH);
 //   console.log(products);
 
   const [index, setIndex] = useState(0);
   //   let ref = useRef();
   
-  const ref = useRef(null);
+  const ref = useRef<number | null>(null);
   
 
   const handleNext = () => {
-    setIndex((index) => {
-      if (index === DATA_LENGTH - 1) {
-        return 0;
-      } else {
-        return index + 1;
-      }
-    });
+    setIndex((index) => (index === DATA_LENGTH - 1 ? 0 : index + 1));
   };
+
   const handlePrev = () => {
-    setIndex((index) => {
-      if (index === 0) {
-        return DATA_LENGTH - 1;
-      } else {
-        return index - 1;
-      }
-    });
+    setIndex((index) => (index === 0 ? DATA_LENGTH - 1 : index - 1));
   };
 
   useEffect(() => {
-     ref.current = setInterval(handleNext, 3000);
-    console.log(ref.current)
-    return () => {clearInterval( ref.current )};
-  }, []);
+    if (DATA_LENGTH > 0) {
+      ref.current = window.setInterval(handleNext, 3000);
+    }
+    return () => {
+      if (ref.current !== null) {
+        clearInterval(ref.current);
+      }
+    };
+  }, [DATA_LENGTH]);
+
+  if (!products || DATA_LENGTH === 0) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <>
       <div 
-      onMouseEnter={()=>clearInterval(ref.current)}
-      onMouseLeave={() => {ref.current = setInterval(handleNext, 3000)}}
+       onMouseEnter={() => {
+        if (ref.current !== null) {
+          clearInterval(ref.current);
+        }
+      }}
+      onMouseLeave={() => {
+        ref.current = window.setInterval(handleNext, 3000);
+      }}
       
       className="w-full ">
         <Link to={`/product/${products[index].id}`}><img
           className="md:pt-20 pt-32 cursor-pointer mx-auto relative h-[400px] w-1/2"
           src={products[index]?.image}
-          alt=""
+          alt={products[index]?.description || "Product Image"}
         /></Link>
         
         <div
