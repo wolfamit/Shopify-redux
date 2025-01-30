a78a3b9f323d77acca16df418a8a1aa116c3bdb6
Amitabh <sonuambastha117@gmail.com>
2025-01-30 22:22:52 +0530

    p

--- src/App.tsx
+++ src/App.tsx
@@ -12,14 +12,32 @@ import "react-loading-skeleton/dist/skeleton.css";
 import Footer from "./components/Footer";
 import UserModal from "./components/ModalNewUser";
 import { useEffect , useState} from "react";
+import axios from "axios";
 
 function App() {
   const [isUserModalOpen, setIsUserModalOpen] = useState(false);
+  const [cartId, setCartId] = useState();
+  
+  useEffect(() => {
+    // Fetch the cart_id from the backend when the component mounts
+    const fetchCartId = async () => {
+        try {
+            const response = await axios.get('https://cloudcart.vercel.app/cart');
+            setCartId(response.data.cart_id);
+        } catch (error) {
+            console.error('Error fetching cart ID:', error);
+        }
+    };
+
+    console.log('cartId:', cartId);
+
+    fetchCartId();
+}, []);
 
   useEffect(() => {
     const userName = localStorage.getItem("userName");
     const userMobile = localStorage.getItem("userMobile");
-
+    
     if (!userName || !userMobile) {
       setIsUserModalOpen(true);
     }
