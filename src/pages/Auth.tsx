282729fb7452ce85bc1aafa74f2013bd09ce8b54
Amitabh <sonuambastha117@gmail.com>
2025-03-20 11:57:28 +0530

    fina2l

--- src/pages/Auth.tsx
+++ src/pages/Auth.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { useNavigate, useParams } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 
 const Auth = () => {
